0db9f15f19ea92e2c79c2bf57af5de1ad6d6d0b3
kimleon <kimleon@mit.edu>
2015-01-17 18:17:26 -0500

    playlists in dict

--- playlist_result.py
+++ playlist_result.py
@@ -1,23 +1,13 @@
 
-def find_playlist((pos_or_neg, emotion)):
-	url = "default"
-	if pos_or_neg == "positive":
-		if emotion == "romantic":
-			url = "https://play.spotify.com/user/spotify/playlist/08vPKM3pmoyF6crB2EtASQ"
-		elif emotion == "athletic":
-			url = "https://play.spotify.com/user/spotify/playlist/5p9ILyu1wb4KKHORoXU8nb"
-		elif emotion == "hustle":
-			url = "https://play.spotify.com/user/1239967792/playlist/3lxpmR5ptfRFp1DcEqhenT"
-		elif emotion == "peaceful":
-			url = "https://play.spotify.com/user/spotify/playlist/7jq9hVhkNUyFLN1XivhLvK"
-	else:
-		if emotion == "romantic":
-			url = "https://play.spotify.com/user/spotify_netherlands/playlist/3GbdQ33cuuC7fR0xibxir0"
-		elif emotion == "athletic":
-			url = "https://play.spotify.com/user/spotify/playlist/3ZSsJYq3of05Rmu3ck5tkO"
-		elif emotion == "hustle":
-			url = "https://play.spotify.com/user/spotify/playlist/2ujjMpFriZ2nayLmrD1Jgl"
-		elif emotion == "peaceful":
-			url = "https://play.spotify.com/user/gobins/playlist/2swVZYX6SVh5HbmOXEOg5P"
-	
+playlist_dict = {("positive", "romantic"): "https://play.spotify.com/user/spotify/playlist/08vPKM3pmoyF6crB2EtASQ", 
+					("positive", "athletic"): "https://play.spotify.com/user/spotify/playlist/5p9ILyu1wb4KKHORoXU8nb",
+					("positive", "hustle"): "https://play.spotify.com/user/1239967792/playlist/3lxpmR5ptfRFp1DcEqhenT",
+					("positive", "peaceful"): "https://play.spotify.com/user/spotify/playlist/7jq9hVhkNUyFLN1XivhLvK",
+					("negative", "romantic"): "https://play.spotify.com/user/spotify_netherlands/playlist/3GbdQ33cuuC7fR0xibxir0",
+					("negative", "athletic"): "https://play.spotify.com/user/spotify/playlist/3ZSsJYq3of05Rmu3ck5tkO",
+					("negative", "hustle"): "https://play.spotify.com/user/spotify/playlist/2ujjMpFriZ2nayLmrD1Jgl",
+					("negative", "peaceful"): "https://play.spotify.com/user/gobins/playlist/2swVZYX6SVh5HbmOXEOg5P",}
+
+def find_playlist(mood):
+	url = playlist_dict.get(mood)
 	return url
